6f4cf29ae8f5af0203d52e4cf759de4452bb2b11
Alexander <alexandrsuchkov67@gmail.com>
2022-07-15 22:51:31 +0300

    Updated babel config

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,15 @@
-module.exports = api => {
+module.exports = (api) => {
   api.cache.using(() => process.env.NODE_ENV);
 
-  const presets = [
-      "@babel/preset-env",
-      "@babel/preset-react"
+  const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    process.env.NODE_ENV === 'development' && 'react-refresh/babel',
   ];
 
+  const presets = ['@babel/preset-env', '@babel/preset-react'];
+
   return {
-      presets
-  }
-}
+    presets,
+    plugins,
+  };
+};
