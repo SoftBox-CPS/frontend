1ccd48ec98916b23ed6dd1bbef1ff3314233e111
Alexander <alexandrsuchkov67@gmail.com>
2022-09-20 21:21:35 +0300

    Updated App component & added header & footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,27 @@ import {
   Navigate,
 } from 'react-router-dom';
 
+import Header from 'components/Header';
+import Footer from 'components/Footer';
 import Room from 'pages/Room';
 
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path='/'>
-          <Route path='home' element={<h1>Home</h1>} />
+          <Route path='about' element={<h1>About</h1>} />
+          <Route path='contacts' element={<h1>Contacts</h1>} />
+          <Route path='' element={<h1>Home</h1>} />
+          <Route path='info' element={<h1>Info</h1>} />
+
           <Route path='room' element={<Room />} />
 
           <Route path='*' element={<Navigate to='home' replace />} />
         </Route>
       </Routes>
+      <Footer />
     </Router>
   );
 };
