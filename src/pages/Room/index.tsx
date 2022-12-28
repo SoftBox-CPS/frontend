43666a04d631755c267185f3cf0e874ea90e8b04
Alexander <alexandrsuchkov67@gmail.com>
2022-12-28 17:09:19 +0300

    Added chat to room page

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Chat from './components/Chat';
 
 import styles from './Room.module.scss';
 
-const Room = () => {
-  return <div className={styles.container}></div>;
+const Room: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <Chat />
+    </div>
+  );
 };
 
 export default Room;
