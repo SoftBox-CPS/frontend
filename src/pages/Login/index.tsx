d27b8566d0ffc8d11feced33e1df3a9f87f52226
Alexander <alexandrsuchkov67@gmail.com>
2023-03-30 21:33:14 +0300

    Fixed imports for modules

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,17 +1,18 @@
 import React, { FC } from "react";
-import Footer from "components/Footer";
-import Header from "components/Header";
-import styles from "./login-page.module.scss";
+import styles from "./Login.module.scss";
 
 const LoginPage: FC = () => {
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Вход</h2>
+
       <form name="login" className={styles.form}>
         <label>Логин</label>
         <input id="login" type="text" name="login" placeholder="Логин" />
+
         <label>Пароль</label>
         <input id="password" type="text" name="password" placeholder="Пароль" />
+        
         <button type="button" className={styles.button}>Войти</button>
       </form>
     </div>
