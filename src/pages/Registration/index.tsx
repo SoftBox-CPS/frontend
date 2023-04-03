d96f45eb1a4898a9606573f6da0442e1ab3c6492
Alexander <alexandrsuchkov67@gmail.com>
2023-04-03 11:10:38 +0300

    Renamed modules for Login and Registration pages

--- src/pages/Registration/index.tsx
+++ src/pages/Registration/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import styles from './Registration-page.module.scss';
+import styles from './Registration.module.scss';
 
 const RegistrationPage: FC = () => {
   return (
