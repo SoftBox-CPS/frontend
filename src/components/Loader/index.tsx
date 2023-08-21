8c1e3af3a13886a9a7974c4abe6f5cb812ad4995
Alexandr Suchkov <alexandrsuchkov67@gmail.com>
2023-08-21 23:33:30 +0300

    Added signalR & loader component & chat logic

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from './Loader.module.scss';
+
+const Loader: React.FC = () => {
+  return (
+    <div className={styles.LdsDualRing}></div>
+  );
+};
+  
+export default Loader;
